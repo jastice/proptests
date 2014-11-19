1a97b6e9a312629c5e5f341b1f21a8db5d06c662
Justin Käser <j.kaeser@clueda.com>
2014-11-19 17:22:14 +0100

    prop test for weirdly behaved object

---
+++ src/test/scala/com/clueda/props/WeirdlyBehavedObjectSpec.scala
@@ -0,0 +1,20 @@
+package com.clueda.props
+
+import org.scalatest.PropSpec
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+
+/**
+ * We have a weirdly behaved object with internal side-effects.
+ */
+class WeirdlyBehavedObjectSpec extends PropSpec with GeneratorDrivenPropertyChecks {
+
+  property("Weirdly Behaved Object accumulates numbers (hopefully!)") {
+    forAll { (n: Int) =>
+      val before = WeirdlyBehavedObject.acc
+      WeirdlyBehavedObject.accumulate(n)
+      val after = WeirdlyBehavedObject.acc
+
+      assert( after == before+n)
+    }
+  }
+}
